8aaadd83aaf1a7538764ff37c16bcdf27cce6f07
Jet Rico <jetriconew@gmail.com>
2020-04-03 18:05:22 +0800

    [crawler-youtube] schedule config, nodemon, firestore

--- services/crawler-youtube/src/index.js
+++ services/crawler-youtube/src/index.js
@@ -1,3 +1,4 @@
+const config = require('config')
 const schedule = require('node-schedule')
 
 const checkLive = require('./tasks/check-live')
@@ -5,23 +6,23 @@ const updateChannels = require('./tasks/update-channels')
 const crawlVideos = require('./tasks/crawl-videos')
 const crawlComments = require('./tasks/crawl-comments')
  
-// Check livestream statuses EVERY MINUTE
-schedule.scheduleJob('0 * * * * *', function(){
+// Check livestream statuses
+schedule.scheduleJob(config.timings['check-live'], function(){
   checkLive()
 })
 
-// Update channel information and stats EVERY DAY
-schedule.scheduleJob('0 0 0 * * *', function(){
+// Update channel information and stats
+schedule.scheduleJob(config.timings['update-channels'], function(){
   updateChannels()
 })
 
-// Re-fetch list of videos EVERY HOUR
-schedule.scheduleJob('0 0 * * * *', function(){
+// Re-fetch list of videos
+schedule.scheduleJob(config.timings['crawl-videos'], function(){
   crawlVideos()
 })
 
-// Fetch comments for new videos EVERY FIVE MINUTES
-schedule.scheduleJob('0 */5 * * * *', function(){
+// Fetch comments for new videos
+schedule.scheduleJob(config.timings['crawl-comments'], function(){
   crawlComments()
 })
 
--- services/crawler-youtube/src/tasks/check-live.js
+++ services/crawler-youtube/src/tasks/check-live.js
@@ -1,9 +1,39 @@
+const {Firestore} = require('@google-cloud/firestore');
+
 module.exports = function() {
   (async function(){
     console.log('checkLive() START')
     let result = {}
+
+    const firestore = new Firestore({ keyFilename: 'gcp-key.json' })
+
+    // GET
+    // const foobarRef = firestore.doc('test/foobar')
+    // const foobarData = await foobarRef.get()
+    // console.log('foobarData.exists', foobarData.exists)
+    // console.log('foobarData', foobarData.data()))
+
+    // UPDATE
+    // const foobarRef = firestore.doc('test/foobar')
+    // let foobarUp = await foobarRef.update({ b: 2 })
+    // console.log('foobarUp', foobarUp)
+
+    // CREATE
+    // const foobarRef = firestore.doc('test/helloworld')
+    // const foobarAdd = await foobarRef.create({ a: 3, b: 4 })
+    // console.log('foobarAdd', foobarAdd)
     
-    // logic
+    // FETCH
+    // const testCol = firestore.collection('test')
+    // const testList = await testCol.where('b', '==', 4).get()
+    // testList.forEach(foobarData => {
+    //   console.log('foobarData', foobarData.ref.parent.id, foobarData.id, foobarData.data())
+    // })
+
+    // DELETE
+    // const foobarRef = firestore.doc('test/helloworld')
+    // let foobarDel = await foobarRef.delete()
+    // console.log('foobarDel', foobarDel)
     
     console.log('checkLive() SUCCESS', result)
   })()
