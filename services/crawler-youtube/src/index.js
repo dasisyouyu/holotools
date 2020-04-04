9236773827f05b94c29d3c7af1eeb082fd9a41b9
Jet Rico <jetriconew@gmail.com>
2020-04-05 04:16:36 +0800

    [crawler-youtube] update scheduling

--- services/crawler-youtube/src/index.js
+++ services/crawler-youtube/src/index.js
@@ -8,26 +8,34 @@ const videosStatus = require('./tasks/videos-status')
 const videosLive = require('./tasks/videos-live')
 const crawlComments = require('./tasks/crawl-comments')
 
-crawlChannels()
+// Update channel information and get today's stats
+schedule.scheduleJob(config.timings['crawl-channels'], function(){
+  crawlChannels()
+})
 
-// Check livestream statuses
-// schedule.scheduleJob(config.timings['check-live'], function(){
-//   checkLive()
-// })
+// Gets all videos from all channels since the beginning
+schedule.scheduleJob(config.timings['crawl-videos'], function(){
+  crawlVideos()
+})
 
-// Update channel information and stats
-// schedule.scheduleJob(config.timings['update-channels'], function(){
-//   updateChannels()
-// })
+// Gets latest videos from each channel through YouTube XML feed
+schedule.scheduleJob(config.timings['videos-feed'], function(){
+  videosFeed()
+})
 
-// Re-fetch list of videos
-// schedule.scheduleJob(config.timings['crawl-videos'], function(){
-//   crawlVideos()
-// })
+// Checks the status of newly added videos if they're past, upcoming, or live
+schedule.scheduleJob(config.timings['videos-status'], function(){
+  videosStatus()
+})
 
-// Fetch comments for new videos
-// schedule.scheduleJob(config.timings['crawl-comments'], function(){
-//   crawlComments()
-// })
+// Checks status of known live videos using heartbeat
+schedule.scheduleJob(config.timings['videos-live'], function(){
+  videosLive()
+})
+
+// Gets comments from videos to check for timestamps
+schedule.scheduleJob(config.timings['crawl-comments'], function(){
+  crawlComments()
+})
 
 console.log('RUNNING YOUTUBE CRAWLER...')
